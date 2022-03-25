3846b19dbdd3c09a96e5aa727ad66e13fa1bf7ee
John Edvard Reiten <johnedvard89@gmail.com>
2022-03-25 19:45:26 +0900

    refactor: Include default hero in chracters array

--- src/app/characters/character-selection/character-selection.component.ts
+++ src/app/characters/character-selection/character-selection.component.ts
@@ -11,14 +11,23 @@ import { NFT_SERIES_MIRROR_CRYSTALS } from 'src/app/shared/nearUtil';
   styleUrls: ['./character-selection.component.sass'],
 })
 export class CharacterSelectionComponent implements OnInit {
+  DEFAULT_HERO_ID = 'heroDefault';
   // TODO (johnedvard) read from localstorage (or contract) to get last selected ID.
-  selectedCandidateId = 'heroDefault'; // Candidate
-  currentCharId = 'heroDefault'; // Currently selected character,
-  extraCharacters = [
+  selectedCandidateId = this.DEFAULT_HERO_ID; // Candidate
+  currentCharId = this.DEFAULT_HERO_ID; // Currently selected character,
+  characters = [
+    {
+      id: this.DEFAULT_HERO_ID,
+      imgSrc:
+        'assets/platform_metroidvania/herochar sprites(new)/herochar_jump_up_anim.gif',
+      isOwned: true,
+      name: 'Near Nut',
+    },
     {
       id: NFT_SERIES_MIRROR_CRYSTALS,
       imgSrc: 'assets/mirror_crystals/MirrorCrystalsHero16x16.png',
       isOwned: false,
+      name: 'Mirror Crystal Hero',
     },
   ];
   account: Account;
@@ -56,7 +65,8 @@ export class CharacterSelectionComponent implements OnInit {
   }
 
   private getNftCharacters() {
-    this.extraCharacters.forEach((c) => {
+    this.characters.forEach((c) => {
+      if (c.id === this.DEFAULT_HERO_ID) return;
       // TODO (johnedvard) add pagination system
       this.nearService.getNftTokensBySeries(c.id).then((res) => {
         // Check if current user owns any tokens in the series
