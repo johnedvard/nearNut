add755279706b22a9db1e37902a4176b851c3133
John Edvard Reiten <johnedvard89@gmail.com>
2022-04-03 11:47:47 +0900

    feat: Build for itchIO and support loading angular with nested children path

--- src/app/level-editor/level-editor-routing.module.ts
+++ src/app/level-editor/level-editor-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LevelEditorComponent } from './level-editor/level-editor.component';
 
-const routes: Routes = [{ path: '', component: LevelEditorComponent }];
+const routes: Routes = [
+  { path: ':levelId', component: LevelEditorComponent },
+  { path: '', component: LevelEditorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
